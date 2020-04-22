560bc5af3d711dae058b58d1434a616c50f686b5
Nitish Harsoor <55267125+nitishkmr005@users.noreply.github.com>
2020-04-22 05:44:53 +0530

    Update status_site.py

--- status_site.py
+++ status_site.py
@@ -3,6 +3,7 @@ from forms import  AddForm , DelForm, UpdForm
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 app = Flask(__name__)
 # Key for Forms
 #app.config['SECRET_KEY'] = 'mysecretkey'
@@ -12,7 +13,7 @@ app = Flask(__name__)
         # SQL DATABASE AND MODELS
 
 ##########################################
-basedir = os.path.abspath(os.path.dirname(__file__))
+#basedir = os.path.abspath(os.path.dirname(__file__))
 # For SQLite =>
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
